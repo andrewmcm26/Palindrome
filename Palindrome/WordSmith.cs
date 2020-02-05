d413c33761375e57000ba7890dc4489d967a9f19
Andrew McMahon <andrewmcm26@gmail.com>
2020-02-05 12:43:02 -0600

    Intial Commit, Unit Tests Included

---
+++ Palindrome/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+namespace Palindrome
+
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string word)
+        {
+            //var ch = word.ToArray();
+            //Array.Reverse(ch);
+            //var rev = new string (ch);
+            //return word == rev;
+
+            return word.SequenceEqual(word.Reverse());
+
+        }
+    }
+}
---
+++ PalindromeTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using Palindrome;
+using Xunit;
+
+namespace PalindromeTests
+{
+    public class UnitTest1
+    {
+
+        [Theory]
+        [InlineData ("racecar", true)]
+        [InlineData ("month", false)]
+        [InlineData ("a", true)]
+        [InlineData ("", true)]
+
+        public void PalindromeTester(string word, bool expected)
+        {
+            //Arrange
+            var wordSmith = new WordSmith();
+
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
